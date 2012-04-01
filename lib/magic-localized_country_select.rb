a190a8623ac305b3e9e85227905987a0a0311fa3
Lorenzo Tello <ltello@belor.jazztel.es>
2012-04-01 14:14:08 +0200

    country codes downcase all over the place

--- lib/magic-localized_country_select.rb
+++ lib/magic-localized_country_select.rb
@@ -15,7 +15,7 @@ require 'rake'
 #
 # Uses the Rails internationalization framework (I18n) for translating the names of countries.
 #
-# Use Rake task <tt>rake import:country_select 'de'</tt> for importing country names
+# Use Rake task <tt>rake import:country_select LOCALE=de</tt> for importing country names
 # from Unicode.org's CLDR repository (http://www.unicode.org/cldr/data/charts/summary/root.html)
 #
 # Code adapted from Rails' default +country_select+ plugin (previously in core)
@@ -28,22 +28,22 @@ module MagicLocalizedCountrySelect
     # for <tt><option></tt> tags
     def localized_countries_array(options = {})
       if options[:description] == :abbreviated
-        I18n.translate(:countries).map { |key, value| [key.to_s.upcase] }.sort_by { |country| country.first }
+        I18n.translate(:countries).map { |key, value| [key.to_s] }.sort_by { |country| country.first }
       else
-        I18n.translate(:countries).map { |key, value| [value, key.to_s.upcase] }.sort_by { |country| country.first }
+        I18n.translate(:countries).map { |key, value| [value, key.to_s] }.sort_by { |country| country.first }
       end
     end
 
     # Return array with codes and localized country names for array of country codes passed as argument
     # == Example
-    #   priority_countries_array([:TW, :CN])
-    #   # => [ ['Taiwan', 'TW'], ['China', 'CN'] ]
+    #   priority_countries_array([:tw, :cn])
+    #   # => [ ['Taiwan', 'tw'], ['China', 'cn'] ]
     def priority_countries_array(country_codes = [], options = {})
       if options[:description] == :abbreviated
-        country_codes.map { |code| [code.to_s.upcase] }
+        country_codes.map { |code| [code.to_s] }
       else
         countries = I18n.translate(:countries)
-        country_codes.map { |code| [countries[code.to_s.upcase.to_sym], code.to_s.upcase] }
+        country_codes.map { |code| [countries[code.to_s.to_sym], code.to_s] }
       end
     end
   end
